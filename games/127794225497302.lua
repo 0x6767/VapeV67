5b5ea7e14ee366d36e07bbb16d497ff177496b17
ryx67 <rtho49401@gmail.com>
2026-02-13 22:49:34 +0700

    refactor(games): align fish silent aim script with universal style

--- games/127794225497302.lua
+++ games/127794225497302.lua
@@ -1,7 +1,6 @@
 local run = function(func)
 	func()
 end
-
 local cloneref = cloneref or function(obj)
 	return obj
 end
@@ -10,35 +9,22 @@ local unpack = table.unpack or unpack
 local playersService = cloneref(game:GetService('Players'))
 local inputService = cloneref(game:GetService('UserInputService'))
 
-local gameCamera = workspace.CurrentCamera
+local gameCamera = workspace.CurrentCamera or workspace:FindFirstChildWhichIsA('Camera')
 local lplr = playersService.LocalPlayer
 local vape = shared.vape
 
+local function notif(...)
+	return vape:CreateNotification(...)
+end
+
 local function getFishFolder()
 	local gameFolder = workspace:FindFirstChild('Game')
-	local fishFolder = gameFolder and gameFolder:FindFirstChild('Fish')
+	if not gameFolder then return end
+	local fishFolder = gameFolder:FindFirstChild('Fish')
 	return fishFolder and fishFolder:FindFirstChild('client') or nil
 end
 
 local function getToolEvent()
-	local debris = workspace:FindFirstChild('debris')
-	if debris then
-		local playerHolder = debris:FindFirstChild(lplr.Name)
-		local playerTool = playerHolder and playerHolder:FindFirstChild('Tool')
-		local playerEvent = playerTool and playerTool:FindFirstChild('Event')
-		if playerEvent and playerEvent:IsA('RemoteEvent') then
-			return playerEvent
-		end
-
-		for _, holder in debris:GetChildren() do
-			local tool = holder:FindFirstChild('Tool')
-			local remote = tool and tool:FindFirstChild('Event')
-			if remote and remote:IsA('RemoteEvent') then
-				return remote
-			end
-		end
-	end
-
 	local character = lplr.Character
 	if character then
 		for _, tool in character:GetChildren() do
@@ -62,33 +48,41 @@ local function getToolEvent()
 			end
 		end
 	end
+
+	local debris = workspace:FindFirstChild('debris')
+	if not debris then return end
+	for _, holder in debris:GetChildren() do
+		local tool = holder:FindFirstChild('Tool')
+		local remote = tool and tool:FindFirstChild('Event')
+		if remote and remote:IsA('RemoteEvent') then
+			return remote
+		end
+	end
 end
 
 local function getClosestFish(maxFov)
 	local fishFolder = getFishFolder()
-	if not fishFolder then
-		return nil
-	end
+	if not fishFolder then return end
 
 	local mousePosition = inputService:GetMouseLocation()
-	local closestRoot
-	local closestDistance = maxFov > 0 and maxFov or math.huge
+	local fishRoot
+	local fishDistance = maxFov > 0 and maxFov or math.huge
 
 	for _, fish in fishFolder:GetChildren() do
 		local root = fish:FindFirstChild('RootPart')
 		if root and root:IsA('BasePart') then
-			local screen, visible = gameCamera:WorldToViewportPoint(root.Position)
-			if visible then
-				local distance = (Vector2.new(screen.X, screen.Y) - mousePosition).Magnitude
-				if distance < closestDistance then
-					closestDistance = distance
-					closestRoot = root
+			local screenPosition, onScreen = gameCamera:WorldToViewportPoint(root.Position)
+			if onScreen then
+				local checkDistance = (Vector2.new(screenPosition.X, screenPosition.Y) - mousePosition).Magnitude
+				if checkDistance < fishDistance then
+					fishDistance = checkDistance
+					fishRoot = root
 				end
 			end
 		end
 	end
 
-	return closestRoot
+	return fishRoot
 end
 
 run(function()
@@ -100,21 +94,11 @@ run(function()
 	FishSilentAim = vape.Categories.Blatant:CreateModule({
 		Name = 'FishSilentAim',
 		Function = function(callback)
-			if not callback then
-				return
-			end
-
-			if hooked then
-				return
-			end
+			if not callback then return end
+			if hooked then return end
 
 			if not hookmetamethod or not getnamecallmethod or not newcclosure then
-				vape:CreateNotification(
-					'FishSilentAim',
-					'Exploit tidak support hookmetamethod/getnamecallmethod.',
-					10,
-					'alert'
-				)
+				notif('FishSilentAim', 'Executor is missing hookmetamethod support.', 10, 'alert')
 				task.defer(function()
 					if FishSilentAim.Enabled then
 						FishSilentAim:Toggle()
@@ -126,28 +110,28 @@ run(function()
 			hooked = true
 			oldNamecall = hookmetamethod(game, '__namecall', newcclosure(function(self, ...)
 				local method = getnamecallmethod()
-				local args = {...}
-
-				if FishSilentAim.Enabled and method == 'FireServer' and args[1] == 'use' then
-					local event = getToolEvent()
-					if event and self == event then
-						local fishRoot = getClosestFish(FOV.Value)
-						if fishRoot then
-							local cameraPosition = gameCamera.CFrame.Position
-							local delta = fishRoot.Position - cameraPosition
-							if delta.Magnitude > 0 then
-								args[2] = fishRoot.Position
-								args[3] = delta.Unit
-								return oldNamecall(self, unpack(args))
+				if FishSilentAim.Enabled and method == 'FireServer' then
+					local args = {...}
+					if args[1] == 'use' then
+						local event = getToolEvent()
+						if event and self == event then
+							local fish = getClosestFish(FOV.Value)
+							if fish then
+								local cameraPosition = gameCamera.CFrame.Position
+								local direction = fish.Position - cameraPosition
+								if direction.Magnitude > 0 then
+									args[2] = fish.Position
+									args[3] = direction.Unit
+									return oldNamecall(self, unpack(args))
+								end
 							end
 						end
 					end
 				end
-
 				return oldNamecall(self, ...)
 			end))
 		end,
-		Tooltip = 'Silent aim ke ikan terdekat di workspace.Game.Fish.client'
+		Tooltip = 'Silently aims your use remote to the closest fish.'
 	})
 
 	FOV = FishSilentAim:CreateSlider({
