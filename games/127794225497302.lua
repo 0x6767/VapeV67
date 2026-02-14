c678a73af47dc592ac7aaf9e8c80da6ba0330e2d
ryx67 <rtho49401@gmail.com>
2026-02-14 09:08:38 +0700

    fix(games): keg

--- games/127794225497302.lua
+++ games/127794225497302.lua
@@ -90,17 +90,14 @@ run(function ()
 								catchui.Gradient.BackgroundColor3 = Color3.fromRGB(136, 194, 89)
 							end
 						end
-						local greenp = catchui.Green.AbsolutePosition + catchui.Green.AbsoluteSize / 2
-						local fishp = catchui.Marker.Fish.AbsolutePosition + catchui.Marker.Fish.AbsoluteSize / 2
-
-						if (greenp - fishp).Magnitude > 3 then
-							mouse1press()
-							info = true
-						else
-							if info then
-								mouse1release()
-								info = false
-							end
+						greenp = catchui.Green.AbsolutePosition + catchui.Green.AbsoluteSize / 2
+                        fishp = catchui.Marker.Fish.AbsolutePosition + catchui.Marker.Fish.AbsoluteSize / 2
+						
+						local diff = greenp - fishp
+                        local len = math.sqrt(diff.X * diff.X + diff.Y * diff.Y)
+						
+						if len > 0 then
+							catchui.Green.Position = catchui.Green.Position + UDim2.new(0, diff.X / len * 5, 0, diff.Y / len * 5)
 						end
                     end
 					task.wait()
