423fac87430159a9a42ed0e38bc95f32aa52a8c6
ryx67 <rtho49401@gmail.com>
2026-02-14 11:26:39 +0700

    fix(game): lol

--- games/127794225497302.lua
+++ games/127794225497302.lua
@@ -23,57 +23,45 @@ local isnetworkowner = identifyexecutor and table.find({'AWP', 'Nihon'}, ({ident
 end
 local gameCamera = workspace.CurrentCamera
 local lplr = playersService.LocalPlayer
-local moduleCategories = {'Combat', 'Blatant', 'Render', 'Utility', 'World', 'Minigames'}
+local assetfunction = getcustomasset
 
 local vape = shared.vape
+local entitylib = vape.Libraries.entity
+local targetinfo = vape.Libraries.targetinfo
 local lucide = vape.Libraries.lucide
 
-local removeModules = {}
-for moduleName, module in vape.Modules do
-	for _, category in next, moduleCategories do
-		if module.Category == category then
-			table.insert(removeModules, moduleName)
+local hmm = {}
+for k, v in vape.Modules do
+	for _, categories in next, {'Combat', 'Blatant', 'Render', 'Utility', 'World', 'Minigames'} do
+		if v.Category == categories then
+			table.insert(hmm, k)
 		end
 	end
 end
-for _, moduleName in next, removeModules do
-	vape:Remove(moduleName)
+for _, china in next, hmm do
+	vape:Remove(china)
 end
 
 local farm = vape:CreateCategory({
 	Name = 'Farm',
-	Icon = lucide and lucide:GetAssetId('fishing-hook', 'fish') or ''
+	Icon = lucide:get('fishing-hook')
 })
 
-
-run(function()
-	local Catch
-	local Range
-	local fish
+run(function ()
+    local Catch
+    local Range
+    local fish
 
 	local function getFish()
-		local target = nil
-		local character = lplr.Character
-		if not character or not character.HumanoidRootPart then
-			return nil
-		end
-
-		local suc, fishes = pcall(function()
-			return workspace.Game.Fish.client:GetChildren()
-		end)
-		if not suc then
-			return nil
-		end
-
-		for _, fishObj in next, fishes do
-			if fishObj.RootPart then
-				local magnitude = (fishObj.RootPart.Position - character.HumanoidRootPart.Position).Magnitude
-				if magnitude < Range.Value then
-					target = fishObj
-				end
+		local Target = nil
+		
+		for i, v in next, workspace.Game.Fish.client:GetChildren() do
+			local Magnitude = (v.RootPart.Position - lplr.Character.HumanoidRootPart.Position).Magnitude
+			if Magnitude < Range.Value then
+				Target = v
 			end
 		end
-		return target
+		return Target
 	end
 	
 	Catch = farm:CreateModule({
@@ -81,12 +69,11 @@ run(function()
 		Function = function(callback)
 			if callback then
 				repeat
-					fish = getFish()
-					if fish then
-						replicatedStorage.common.packages.Knit.Services.HarpoonService.RF.StartCatching:InvokeServer(
-							fish.Name
-						)
-					end
+					fish = getFish(Range)
+                    if fish then
+                        replicatedStorage.common.packages.Knit.Services.HarpoonService.RF.StartCatching:InvokeServer(
+                        fish.Name)
+                    end
 					task.wait()
 				until not Catch.Enabled
 			end
@@ -102,41 +89,38 @@ run(function()
 	})
 end)
 
-run(function()
-	local Minigame
-	local Gradient
+run(function ()
+    local Minigame
+    local Color
 	local Size
+	local catchui = lplr.PlayerGui.Main.CatchingBar.Frame.Bar.Catch
 
 	Minigame = farm:CreateModule({
 		Name = 'Auto Minigame',
 		Function = function(callback)
 			if callback then
 				repeat
-					local catchui
-					local suc = pcall(function()
-						catchui = lplr.PlayerGui.Main.CatchingBar.Frame.Bar.Catch
-					end)
-					if suc and catchui and catchui.Parent then
-						catchui.Green.Size = UDim2.new(0, 0, Size.Value, 0)
+                    if catchui.Parent then
+						catchui.Green.Size = UDim2.new(0, 0, Size, 0)
 						if Gradient.Enabled then
 							catchui.Gradient.BackgroundColor3 = Color3.fromRGB(136, 194, 89)
 						end
-					end
+                    end
 					task.wait()
 				until not Minigame.Enabled
 			end
-		end,
+        end,
 		Tooltip = 'take a guess'
-	})
+    })
 	Gradient = Minigame:CreateToggle({
-		Name = 'Gradient',
+        Name = 'Gradient',
 		Tooltip = 'make your bar turn green'
-	})
+    })
 	Size = Minigame:CreateSlider({
 		Name = 'Size',
 		Min = 0,
 		Max = 1,
-		Default = 1,
+		Default = 10,
 		Suffix = ''
 	})
 end)
